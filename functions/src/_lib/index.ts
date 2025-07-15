ca6120ca835e433584f9666f20fd54a45ec73974
Christophe Querton <kertof@gmail.com>
2025-07-15 13:45:12 +0200

    simplify the code

--- functions/src/_lib/index.ts
+++ functions/src/_lib/index.ts
@@ -2,10 +2,12 @@ import { filePaths, Hash, hashFactory, sumAndDiff } from "avatars-utils";
 import path from "path";
 import sharp from "sharp";
 
-export const combine = (face: Face) =>
-  sharp(face.eyes)
-    .composite([{ input: face.mouth }, { input: face.nose }])
-    .flatten({ background: face.color });
+interface Face {
+  color: string;
+  eyes: string;
+  nose: string;
+  mouth: string;
+}
 
 const imageDir = path.join(__dirname, "_img");
 
@@ -15,42 +17,6 @@ const imageFilePaths = (type: keyof Face): string[] =>
 const eyeImages = imageFilePaths("eyes");
 const noseImages = imageFilePaths("nose");
 const mouthImages = imageFilePaths("mouth");
-
-interface Face {
-  color: string;
-  eyes: string;
-  nose: string;
-  mouth: string;
-}
-
-class FaceFactory {
-  private colorHash: Hash<string>;
-  private eyeHash: Hash<string>;
-  private noseHash: Hash<string>;
-  private mouthHash: Hash<string>;
-
-  constructor(
-    colors: string[],
-    eyes: string[],
-    noses: string[],
-    mouths: string[]
-  ) {
-    this.colorHash = new Hash(colors);
-    this.eyeHash = new Hash(eyes);
-    this.noseHash = new Hash(noses);
-    this.mouthHash = new Hash(mouths, hashFactory(sumAndDiff));
-  }
-
-  public create(string: string): Face {
-    return {
-      color: this.colorHash.get(string),
-      eyes: this.eyeHash.get(string),
-      nose: this.noseHash.get(string),
-      mouth: this.mouthHash.get(string),
-    };
-  }
-}
-
 const defaultColors = [
   "#81bef1",
   "#ad8bf2",
@@ -63,9 +29,21 @@ const defaultColors = [
   "#f6be5d",
 ];
 
-export const defaultFaceFactory = new FaceFactory(
-  defaultColors,
-  eyeImages,
-  noseImages,
-  mouthImages
-);
+const colorHash: Hash<string> = new Hash(defaultColors);
+const eyeHash: Hash<string> = new Hash(eyeImages);
+const noseHash: Hash<string> = new Hash(noseImages);
+const mouthHash: Hash<string> = new Hash(mouthImages, hashFactory(sumAndDiff));
+
+export const createFace = (string: string): Face => {
+  return {
+    color: colorHash.get(string),
+    eyes: eyeHash.get(string),
+    nose: noseHash.get(string),
+    mouth: mouthHash.get(string),
+  };
+};
+
+export const combine = (face: Face) =>
+  sharp(face.eyes)
+    .composite([{ input: face.mouth }, { input: face.nose }])
+    .flatten({ background: face.color });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,26 @@
 import { randomUUID } from "crypto";
 import { onRequest } from "firebase-functions/v2/https";
-import { combine, defaultFaceFactory } from "./_lib";
+import { combine, createFace } from "./_lib";
 
 export const id = onRequest({ timeoutSeconds: 5 }, async (req, res) => {
   const pathId = req.path?.slice(1);
   const id = pathId || randomUUID();
-  const face = defaultFaceFactory.create(id);
+  const face = createFace(id);
   const png = await combine(face).png().toBuffer();
   res.setHeader("Content-Type", "image/png");
+  res.setHeader("X-Genavatar-ID", id);
+
   if (pathId) {
     res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+    res.setHeader("Expires", new Date(Date.now() + 31536000000).toUTCString());
   } else {
     // Set no cache headers for random faces
     res.setHeader(
       "Cache-Control",
       "no-store, no-cache, must-revalidate, max-age=0"
     );
-    res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", "0");
   }
-  // Add X-Genavatar-ID header for caching purposes
-  res.setHeader("X-Genavatar-ID", id);
+
   res.send(png);
 });
