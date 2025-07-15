6a081a862a73345629ed7d9657ddce9bb8728c39
Christophe Querton <kertof@gmail.com>
2025-07-15 13:47:56 +0200

    up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,18 +9,12 @@ export const id = onRequest({ timeoutSeconds: 5 }, async (req, res) => {
   const png = await combine(face).png().toBuffer();
   res.setHeader("Content-Type", "image/png");
   res.setHeader("X-Genavatar-ID", id);
-
-  if (pathId) {
-    res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
-    res.setHeader("Expires", new Date(Date.now() + 31536000000).toUTCString());
-  } else {
-    // Set no cache headers for random faces
-    res.setHeader(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, max-age=0"
-    );
-    res.setHeader("Expires", "0");
-  }
+  res.setHeader(
+    "Cache-Control",
+    pathId
+      ? "public, max-age=31536000, immutable"
+      : "no-store, no-cache, must-revalidate, max-age=0"
+  );
 
   res.send(png);
 });
